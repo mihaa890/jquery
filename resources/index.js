6ae2d98d68521593ff90bc3becd131f2e00f164f
Roxana Popescu <roxanateodorapopescu@yahoo.com>
2022-07-01 23:28:13 +0300

    code review

--- resources/index.js
+++ resources/index.js
@@ -2,13 +2,16 @@ $(function () {
   const entries = $('.cards-wrapper');
   entries.html('');
 
+  // unul din requirement-uri pt acest layout era sa iti populezi tot continutul din array-uri pe fiecare categorie in parte ceea ce vad ca lipseste
   $.getJSON('resources/products.json', function (result) {
+    // ulterior dinamic trebuiau luate categoriile respective, eventual preluate printr-un data attribute
     const vases = result.vases;
     $.map(vases, function (val) {
       const content = JSON.stringify(val);
+      // cand incarci continut dinamic trebuie sa consideri ca ai sa incarci imaginile folosind ca referinta fisierul html, si nu cel js, drept urmare nu a nevoie sa urci un nivel de navigare
       let place = `
       <div class="card-wrapper">
-      <img src="../assets/vases/${val.imgUrl}" class="img-wrapper open" data-content='${content}'>
+      <img src="assets/vases/${val.imgUrl}" class="img-wrapper open" data-content='${content}'>
       <div class="product-description-wrapper">
         <span class="description-wrapper">
           ${val.series}
@@ -25,11 +28,13 @@ $(function () {
     $(".open").on("click", function () {
       $(".popup-overlay, .popup-content").addClass("active");
       $(".left-section").html('');
+      // mare atentie la navigare, vad ca inca sunt probleme, spune-mi daca ai dori sa reluam conceptul, te rog
       $(".left-section").append("<img src='../vases/" + $(this).attr("src") + "' class='left-img-wrapper'>");
       $(".right-section").html('');
 
       const data = $(this).data("content");
 
+      // nu ar trebui sa ai nevoie sa recreezi modalul de fiecare data, acesta poate fi markup static, iar apoi prin selectii sa iti populezi dinamic in functie de produsul selectat
       if (data) {
         let content =
           `<div class="product-description-wrapper">
