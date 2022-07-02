763c05921ae54fcc1f0f84d6c458d0d507e5f451
Lazar Mihaela <mihaela_lazar11@yahoo.ro>
2022-07-02 18:24:19 +0200

    implement click event handle on types menu

--- resources/index.js
+++ resources/index.js
@@ -1,113 +1,120 @@
+const DEFAULT_CATEGORY_SELECTED = "vases";
 $(function () {
   const entries = $('.cards-wrapper');
   entries.html('');
 
-  // unul din requirement-uri pt acest layout era sa iti populezi tot continutul din array-uri pe fiecare categorie in parte ceea ce vad ca lipseste
   $.getJSON('resources/products.json', function (result) {
-    // ulterior dinamic trebuiau luate categoriile respective, eventual preluate printr-un data attribute
-    const vases = result.vases;
-    $.map(vases, function (val) {
-      const content = JSON.stringify(val);
-      // cand incarci continut dinamic trebuie sa consideri ca ai sa incarci imaginile folosind ca referinta fisierul html, si nu cel js, drept urmare nu a nevoie sa urci un nivel de navigare
-      let place = `
-      <div class="card-wrapper">
-      <img src="assets/vases/${val.imgUrl}" class="img-wrapper open" data-content='${content}'>
-      <div class="product-description-wrapper">
-        <span class="description-wrapper">
-          ${val.series}
-          </span>
-          <span class="price-wrapper">
-            ${val.currency}
-            ${val.price}  
-        </span> 
-        </div>
-      </div>`;
+    const categories = result.products;
 
-      entries.append(place);
-    })
-    $(".open").on("click", function () {
-      $(".popup-overlay, .popup-content").addClass("active");
-      $(".left-section").html('');
-      // mare atentie la navigare, vad ca inca sunt probleme, spune-mi daca ai dori sa reluam conceptul, te rog
-      $(".left-section").append("<img src='../vases/" + $(this).attr("src") + "' class='left-img-wrapper'>");
-      $(".right-section").html('');
+    populateCategoryItems(DEFAULT_CATEGORY_SELECTED, categories, entries);
+    openModal(categories, DEFAULT_CATEGORY_SELECTED);
 
-      const data = $(this).data("content");
-
-      // nu ar trebui sa ai nevoie sa recreezi modalul de fiecare data, acesta poate fi markup static, iar apoi prin selectii sa iti populezi dinamic in functie de produsul selectat
-      if (data) {
-        let content =
-          `<div class="product-description-wrapper">
-        <h1 class="description-wrapper">
-          ${data.series}
-          </h1>
-          <span class="price-wrapper">
-            ${data.currency}
-            ${data.price}
-        </span>
-        <p>
-          ${data.description}
-        </p>
-        <div class="atr-description">
-        <div class="desc">
-        <span>
-          Series:
-        </span>
-        <span>
-          ${data.series}
-        </span>
-        </div>
-        <div class="desc">
-        <span>
-          Designer: 
-        </span>
-        <span> ${data.designer} </span> 
-        </div>
-        <div class="desc">
-        <span>
-          SKU: 
-        </span>
-        <span> ${data.sku} </span>
-        </div>
-        <div class="desc">
-        <span>
-          Colour: 
-        </span>
-        <span> ${data.colour} </span>
-        </div>
-        <div class="desc">
-        <span>
-          Material: 
-        </span>
-        <span> ${data.material} </span> 
-        </div>
-        <div class="desc">
-        <span>
-          Height: 
-        </span>
-        <span> ${data.height}${data.unit} </span> 
-        </div>
-    </div>
-      <div class="cart-wrapper">
-      <div>
-        <span>-</span>
-        <span>1</span>
-        <span>+</span>
-    </div>
-        <button class="cart-button">Add to cart</button>
-    </div>
-        </div>`;
-        $(".right-section").append(content);
-      }
+    $("a").click(function () {
+      const productType = this.dataset.type;
+      entries.html('')
+      populateCategoryItems(productType, categories, entries);
+      openModal(categories, productType);
+      $("a").removeClass("selected")
+        $(this).toggleClass("selected");
     });
-    $(".close, .popup-overlay").on("click", function () {
-      $(".popup-overlay, .popup-content").removeClass("active");
-    });
-
   });
 });
+const populateCategoryItems = (categoryKey, categories, entries) => {
+  categories[categoryKey].forEach(function (val) {
 
+    let place = `
+    <div class="card-wrapper">
+    <img src="assets/${categoryKey}/${val.imgUrl}" class="img-wrapper open" data-content='${JSON.stringify(val)}'>
+    <div class="product-description-wrapper">
+      <span class="description-wrapper">
+        ${val.series}
+        </span>
+        <span class="price-wrapper">
+          ${val.currency}
+          ${val.price}  
+      </span> 
+      </div>
+    </div>`;
 
+    entries.append(place);
+    
+  });
+}
 
-
-
+const openModal = (data, productType) => {
+  $(".open").on("click", function () {
+    data = $(this).data("content");
+    $(".popup-overlay, .popup-content").addClass("active");
+    $(".left-section").html('');
+    $(".left-section").append(`<img src="assets/${productType}/${data.imgUrl}" + "' class='left-img-wrapper'>`);
+    $(".right-section").html('');
+    if (data) {
+      let content =
+        `<div class="product-description-wrapper">
+      <h1 class="description-wrapper">
+        ${data.series}
+        </h1>
+        <span class="price-wrapper">
+          ${data.currency}
+          ${data.price}
+      </span>
+      <p>
+        ${data.description}
+      </p>
+      <div class="atr-description">
+      <div class="desc">
+      <span>
+        Series:
+      </span>
+      <span>
+        ${data.series}
+      </span>
+      </div>
+      <div class="desc">
+      <span>
+        Designer: 
+      </span>
+      <span> ${data.designer} </span> 
+      </div>
+      <div class="desc">
+      <span>
+        SKU: 
+      </span>
+      <span> ${data.sku} </span>
+      </div>
+      <div class="desc">
+      <span>
+        Colour: 
+      </span>
+      <span> ${data.colour} </span>
+      </div>
+      <div class="desc">
+      <span>
+        Material: 
+      </span>
+      <span> ${data.material} </span> 
+      </div>
+      <div class="desc">
+      <span>
+        Height: 
+      </span>
+      <span> ${data.height}${data.unit} </span> 
+      </div>
+  </div>
+    <div class="cart-wrapper">
+    <div>
+      <span><img src="assets/minus.png"> </span>
+      <span>1</span>
+      <span><img src="assets/plus.png"> </span>
+  </div>
+      <button class="cart-button">Add to cart</button>
+  </div>
+      </div>`;
+      $(".right-section").append(content);
+    }
+  });
+  $(".close").on("click", function (e) {
+    e.stopPropagation();
+    $(".popup-overlay, .popup-content").removeClass("active");
+  });
+}
